5e5adaebb1f56be7cd8cc3289ff94d7b51b34399
asakidon <asakidon@corcentric.com>
2023-11-04 19:56:28 +0200

    Create Converter

---
+++ src/Converter.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Converter {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Please enter distance in miles:");
+        double distanceInMiles = sc.nextDouble();
+
+        System.out.println(distanceInMiles + " miles = " + convertToKm(distanceInMiles) + " km");
+    }
+
+    private static double convertToKm(double distanceInMiles) {
+        return distanceInMiles * 1.60934;
+    }
+}
